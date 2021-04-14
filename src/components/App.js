83fa6cc139635609b379b1095e45b223b28005d4
Dan <spqjeks@naver.com>
2021-04-14 21:38:19 +0900

    [Start] start our project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Let's get start!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App.js';
+
+import './resource/css/default.css'
+import './resource/css/normalize.css'
+import './resource/css/index.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
