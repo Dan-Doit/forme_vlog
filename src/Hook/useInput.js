f6b6cf08ccadad2e659619fc76764e72b1b96221
Dan <spqjeks@naver.com>
2021-04-16 21:32:43 +0900

    prectice

---
+++ src/Hook/useInput.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+export const useInput = (defaultValue) => {
+    
+    const [value, setValue] = useState(defaultValue);
+
+    const onChange = e => {
+        setValue(e.target.value)
+    }
+
+    return (value, setValue, onChange);
+}
+
+export const useDebounce = (text, delay) => {
+    const [value, setValue] = useState(text)
+
+    useEffect( () => {
+        const timer = setTimeout(() => {
+            setValue(text)
+        }, delay)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [text])
+
+    return value;
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+
 function App() {
+  
+  const [값, 변경함] = useState(0);
+  
+  function Add() {
+    변경함(값 + 1)
+  }
+
+  function Minus() {
+    변경함(값 - 1)
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Let's build our site!
-        </p>
+        <div style={{ fontSize: "60px" }}>{값}</div>
+        <div style={{ display: "inline-block", marginTop: "30px" }} >
+          <button onClick={Add} >더하기 +1</button>
+          <button onClick={Minus}>빼기 -1</button>
+        </div>
       </header>
     </div>
   );
