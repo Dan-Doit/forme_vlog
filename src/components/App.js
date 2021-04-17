70e8a1a3b1ff450564e9b55049ece99f504b02d4
Dan <spqjeks@naver.com>
2021-04-17 12:14:44 +0900

    fix model

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,10 @@ import { useState } from 'react';
 
 function App() {
   
-  const [값, 변경함] = useState(0);
-  
-  function Add() {
-    변경함(값 + 1)
-  }
-
-  function Minus() {
-    변경함(값 - 1)
-  }
-  
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{ fontSize: "60px" }}>{값}</div>
-        <div style={{ display: "inline-block", marginTop: "30px" }} >
-          <button onClick={Add} >더하기 +1</button>
-          <button onClick={Minus}>빼기 -1</button>
-        </div>
+        <p>Let's Start Forme</p>
       </header>
     </div>
   );
