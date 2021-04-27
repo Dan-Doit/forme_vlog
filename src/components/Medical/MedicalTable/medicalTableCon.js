1e519c63d8d7eaf96cc2d193be07623832d64598
sunyeongchoi <sn0716@naver.com>
2021-04-27 23:41:24 +0900

    수녕init

--- src/components/Medical/MedicalTable/medicalTableCon.js
+++ src/components/Medical/MedicalTable/medicalTableCon.js
@@ -4,7 +4,7 @@ import { PersonContext } from '../Medical'
 const MedicalTableCon = () => {
     const peopleInfo = useContext(PersonContext)
     console.log(peopleInfo)
-    return <p>Medicalwewew</p>
+    return <p>수녕 브랜치 시작!</p>
 }
 
 export default MedicalTableCon;
