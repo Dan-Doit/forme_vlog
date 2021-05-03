2168ea6da3b9ebb8d10a995f3fbf2969750cfd2c
sunyeongchoi <sn0716@naver.com>
2021-05-03 23:47:43 +0900

    테이블 제작중

--- src/components/Medical/MedicalTable/medicalTableCon.js
+++ src/components/Medical/MedicalTable/medicalTableCon.js
@@ -4,7 +4,36 @@ import { PersonContext } from '../Medical'
 const MedicalTableCon = () => {
     const peopleInfo = useContext(PersonContext)
     console.log(peopleInfo)
-    return <p>수녕 브랜치 시작!</p>
+    const columns = React.useMemo(
+        () => [
+            {
+                accessor: "name",
+                Header: "Name",
+            },
+            {
+                accessor: "email",
+                Header: "Email",
+            },
+            {
+                accessor: "phone",
+                Header: "Phone",
+            },
+        ],
+        []
+    )
+    const data = React.useMemo(
+        () => 
+        peopleInfo.map((p) => ({
+            id: p.person_id,
+            gender: p.gender_source_value,
+            birth: p.birth_datetime,
+            age: p.age,
+            race: p.race,
+            ethnicity: p.ethnicity,
+            death_data: p.death_date,
+        }))
+    )
+    return <p>수녕 브랜치 시작!!</p>
 }
 
 export default MedicalTableCon;
