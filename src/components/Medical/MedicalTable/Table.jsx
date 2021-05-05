a4559494ff015fe5a9d607456449a04382688cb9
sunyeongchoi <sn0716@naver.com>
2021-05-05 14:54:43 +0900

    테이블 구현중

---
+++ src/components/Medical/MedicalTable/Table.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useTable } from 'react-table';
+
+function Table({ columns, data }) {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({ columns, data })
+
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map((headerGroup) => (
+                    <th {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                        ))}
+                    </th>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => (
+                                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                            ))}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
+
+export default Table;
